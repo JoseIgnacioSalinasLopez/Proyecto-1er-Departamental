142420e2dc49caaf444ae35d3a9b23bb9e69106c
JoseIgnacioSalinasLopez <157532512+JoseIgnacioSalinasLopez@users.noreply.github.com>
2025-09-26 02:11:10 -0600

    Delete frontend directory

--- frontend/practicas/Practica 1 y 2 Prácticas desarrollo Web/Practica 1/JS/logic.js
+++
@@ -1,9 +0,0 @@
-const button = document.getElementById("toggleContact");
-const contact = document.getElementById("contact");
-
-button.addEventListener("click", () => {
-  contact.classList.toggle("hidden");
-  button.textContent = contact.classList.contains("hidden")
-    ? "Mostrar contacto"
-    : "Ocultar contacto";
-});
